766bbb9115f2c57d974a601c4f492be0e923de45
Lapis <45414333+Lapis256@users.noreply.github.com>
2021-02-14 17:31:09 +0900

    -

---
+++ main.js
@@ -0,0 +1,29 @@
+URl = "https://script.google.com/macros/s/AKfycbzfuH8TEwajGUIb48aq2HgIGudigKfPAB-7Na6ulFMv9eSgjJsXnpYwsQ/exec";
+GOOGLE = 0;
+MICROSOFT = 1;
+GBOARD = 2;
+
+async function createBlob (type) {
+	let res = await fetch(URL);
+	let data = await res.json();
+	console.log(JSON.stringify(data));
+
+	switch (type) {
+		case GOOGLE:
+			break;
+		case MICROSOFT:
+			break;
+		case GBOARD:
+			break;
+	}
+
+	return new Blob([JSON.stringify(data)])
+}
+
+function download(type) {
+	let aTag = document.createElement("a");
+	createBlob(type).then((blob) => {
+		aTag.href = window.URL.createObjectURL(blob);
+		aTag.click()
+	});
+}
