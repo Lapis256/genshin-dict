da4a94e127f8cccf4c56b2fda06749876bfe5134
Lapis <45414333+Lapis256@users.noreply.github.com>
2021-02-14 18:45:48 +0900

    -

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-URl = "https://script.google.com/macros/s/AKfycbzfuH8TEwajGUIb48aq2HgIGudigKfPAB-7Na6ulFMv9eSgjJsXnpYwsQ/exec";
+url = "https://script.google.com/macros/s/AKfycbzfuH8TEwajGUIb48aq2HgIGudigKfPAB-7Na6ulFMv9eSgjJsXnpYwsQ/exec";
 GOOGLE = 0;
 MICROSOFT = 1;
 GBOARD = 2;
 
 async function createBlob (type) {
-	let res = await fetch(URL, {mode: "cors"});
+	let res = await fetch(url);
 	let data = await res.json();
-	console.log(JSON.stringify(data));
+
+	let text = ""
+	data.forEach(d => {
+		switch (type) {
+			case MICROSOFT:
+				d[2] = d[2].replace("地名","地名その他");
+				break;
+			case GBOARD:
+				d[2] = "ja-JP";
+				break;
+		}
+		text += (d.join("	") + "\n");
+	});
 
 	switch (type) {
 		case GOOGLE:
@@ -14,15 +26,33 @@ async function createBlob (type) {
 		case MICROSOFT:
 			break;
 		case GBOARD:
-			break;
+			let zip = new Zlib.Zip();
+			zip.addFile(stringToByteArray(text), {
+				filename: stringToByteArray("dictionary.txt")
+			});
+			let compressed = zip.compress();
+			return new Blob([compressed], { 'type': 'application/zip' });
 	}
 
-	return new Blob([JSON.stringify(data)])
+	return new Blob([text])
+}
+
+function stringToByteArray(str) {
+    var array = new (window.Uint8Array !== void 0 ? Uint8Array : Array)(str.length);
+    var i;
+    var il;
+
+    for (i = 0, il = str.length; i < il; ++i) {
+        array[i] = str.charCodeAt(i) & 0xff;
+    }
+
+    return array;
 }
 
 function download(type) {
 	let aTag = document.createElement("a");
 	createBlob(type).then((blob) => {
+		aTag.download = type == GBOARD ? "dict.zip" : "dict.txt"
 		aTag.href = window.URL.createObjectURL(blob);
 		aTag.click()
 	});
